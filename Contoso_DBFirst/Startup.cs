1e6662fcbb39a7fbc2198471da2c4a06abfae254
mim66 <marekmusial2@o2.pl>
2014-07-18 12:30:53 +0200

    https://github.com/mim66/_MVC5_Testy2013.git

---
+++ Contoso_DBFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Contoso_DBFirst.Startup))]
+namespace Contoso_DBFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
