9115b43ba69d2a8be479b9245bd617b8f9696ff9
mim66 <marekmusial2@o2.pl>
2014-07-21 14:10:38 +0200

    MVC5_T: 1.0.16: Obsługa try-catch

--- Contoso_DBFirst/Controllers/StudentController.cs
+++ Contoso_DBFirst/Controllers/StudentController.cs
@@ -10,118 +10,150 @@ using Contoso_DBFirst.Models;
 
 namespace Contoso_DBFirst.Controllers
 {
-    public class StudentController : Controller
-    {
-        private ContosoUniversityEntities db = new ContosoUniversityEntities();
+   public class StudentController : Controller
+   {
+      private ContosoUniversityEntities db = new ContosoUniversityEntities();
 
-        // GET: Student
-        public ActionResult Index()
-        {
-            return View(db.Students.ToList());
-        }
+      // GET: Student
+      public ActionResult Index()
+      {
+         return View(db.Students.ToList());
+      }
 
-        // GET: Student/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+
+      // GET: Student/Details/5
+      public ActionResult Details(int? id)
+      {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Student student = db.Students.Find(id);
+         if (student == null) {
+            return HttpNotFound();
+         }
+         return View(student);
+      }
+
+
+
+      // GET: Student/Create
+      public ActionResult Create()
+      {
+         return View();
+      }
+
+      // POST: Student/Create
+      // To protect from overposting attacks, please enable the specific roperties you want to bind to, for more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+      {
+         try {
+            if (ModelState.IsValid) {
+               db.Students.Add(student);
+               db.SaveChanges();
+               return RedirectToAction("Index");
             }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+         }
+         catch (DataException /* dex */) {
+            //Log the error (uncomment dex variable name and add a line            here to write a log.
+            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+         }
+         return View(student);
+      }
 
-        // GET: Student/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Student/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
 
-            return View(student);
-        }
 
-        // GET: Student/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
 
-        // POST: Student/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
+      // GET: Student/Edit/5
+      public ActionResult Edit(int? id)
+      {
 
-        // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return HttpNotFound();
-            }
-            return View(student);
-        }
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Student student = db.Students.Find(id);
+         if (student == null) {
+            return HttpNotFound();
+         }
+         return View(student);
+      }
 
-        // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
+      // POST: Student/Edit/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+      {
+         try {
+            if (ModelState.IsValid) {
+               db.Entry(student).State = EntityState.Modified;
+               db.SaveChanges();
+               return RedirectToAction("Index");
+            }
+         }
+         catch (DataException /* dex */) {
+            //Log the error (uncomment dex variable name and add a line here to  write a log.
+            ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+         }
+         return View(student);
+      }
+
+
+
+
+      // GET: Student/Delete/5
+      public ActionResult Delete(int? id, bool? saveChangesError=false)
+      {
+         if (id == null) {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         if (saveChangesError.GetValueOrDefault()) {
+            ViewBag.ErrorMessage = "Delete failed. Try again, and if the  problem persists see your system administrator.";
+         }
+         Student student = db.Students.Find(id);
+         if (student == null) {
+            return HttpNotFound();
+         }
+         return View(student);
+      }
+
+      // POST: Student/Delete/5
+      [HttpPost, ActionName("Delete")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id)
+      {
+         try {
+            //mniejsza wydajność 
+            //Student student = db.Students.Find(id);
+            //db.Students.Remove(student);
+            //db.SaveChanges();
+
+            //większa wydajność (wymóg: id z PK)
+            Student studentToDelete = new Student() { ID = id };
+            db.Entry(studentToDelete).State = EntityState.Deleted;
             db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+         }
+         catch (DataException/* dex */) {
+            //Log the error (uncomment dex variable name and add a line      here to write a log.
+            return RedirectToAction("Delete", new { id = id,  saveChangesError = true });
+         }         
+         return RedirectToAction("Index");
+      }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+      
+      
+      
+      
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing) {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+   }
 }
--- Contoso_DBFirst/Models/Enrollment.cs
+++ Contoso_DBFirst/Models/Enrollment.cs
@@ -9,17 +9,21 @@
 
 namespace Contoso_DBFirst.Models
 {
-    using System;
-    using System.Collections.Generic;
-    
-    public partial class Enrollment
-    {
-        public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
-        public int StudentID { get; set; }
-        public Nullable<decimal> Grade { get; set; }
-    
-        public virtual Course Course { get; set; }
-        public virtual Student Student { get; set; }
-    }
+   using System;
+   using System.Collections.Generic;
+
+   //mo¿na korzystaæ z obu zapisów
+   //public enum Grade : int   {      A = 0,      B = 1,      C = 2,      D = 3,      F = 4   }
+   public enum Grade { A,B,C,D,F }
+
+   public partial class Enrollment
+   {
+      public int EnrollmentID { get; set; }
+      public int CourseID { get; set; }
+      public int StudentID { get; set; }
+      public Nullable<Grade> Grade { get; set; }
+
+      public virtual Course Course { get; set; }
+      public virtual Student Student { get; set; }
+   }
 }
