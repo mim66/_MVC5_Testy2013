11b111ba95820b98c0680b195255e4ed28ce6174
mim66 <marekmusial2@o2.pl>
2014-07-29 13:46:30 +0200

    MVC5_T: 1.4.2: Contoso_CodeFirst:  Creating a More Complex Data  Model for an ASP.NET MVC Application

--- Contoso_CodeFirst/Contoso_CodeFirst/Models/Enrollment.cs
+++ Contoso_CodeFirst/Contoso_CodeFirst/Models/Enrollment.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+//rozszerzenie opisu atrybutów
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -9,12 +10,14 @@ namespace ContosoUniversity.Models
 
    public class Enrollment
    {
-      public int EnrollmentID { get; set; }
-      public int CourseID { get; set; }
-      public int StudentID { get; set; }
-      public Grade? Grade { get; set; }
-      public virtual Course Course { get; set; }
-      public virtual Student Student { get; set; }
-
+     public int EnrollmentID { get; set; }
+     public int CourseID { get; set; }
+     public int StudentID { get; set; }
+ 
+     [DisplayFormat(NullDisplayText = "No grade")]
+     public Grade? Grade { get; set; }
+  
+     public virtual Course Course { get; set; }
+     public virtual Student Student { get; set; }
    }
-}
+ }
--- Contoso_CodeFirst/Contoso_CodeFirst/Models/Student.cs
+++ Contoso_CodeFirst/Contoso_CodeFirst/Models/Student.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
+//rozszerzenie opisu atrybutów
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-   public class Student
-   {
-      public int        ID { get; set; }
-      public string     LastName { get; set; } 
-      public string     FirstMidName { get; set; }
-      public DateTime   EnrollmentDate { get; set; }
-
-      public virtual ICollection<Enrollment> Enrollments { get; set; }
-   }
+  public class Student
+  {
+    public int ID { get; set; }
+    
+    [Required]
+    [StringLength(50, MinimumLength=1)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    
+    [Required]
+    [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+    [Column("FirstName")]
+    [Display(Name = "First Name")]
+    public string FirstMidName { get; set; }
+    
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+    ApplyFormatInEditMode = true)]
+    
+    [Display(Name = "Enrollment Date")]
+    public DateTime EnrollmentDate { get; set; }
+    
+    [Display(Name = "Full Name")]
+    public string FullName { get {return LastName + ", " + FirstMidName;}}
+    
+    public virtual ICollection<Enrollment> Enrollments { get; set; }
+  }
 
 
 }
