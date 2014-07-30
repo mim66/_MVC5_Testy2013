16289b70bf07f333f7bfa3b4be511c0f45fc7d1a
mim66 <marekmusial2@o2.pl>
2014-07-30 11:56:42 +0200

    MVC5_T: 1.5.2: Contoso_CodeFirst:  Reading related data: Cz.1 Courses page with Department name - Problemy z wyświetlaniem instruktorów

---
+++ Contoso_CodeFirst/Contoso_CodeFirst/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+////rozszerzenie opisu atrybutów
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+using ContosoUniversity.Models;
+using ContosoUniversity.DAL;
+
+
+namespace ContosoUniversity.ViewModels
+{
+  public class InstructorIndexData
+  {
+    public IEnumerable<Instructor>  Instructors { get; set; }
+    public IEnumerable<Course>      Courses     { get; set; }
+    public IEnumerable<Enrollment>  Enrollments { get; set; }
+  }
+}
