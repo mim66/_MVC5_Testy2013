fbb846e570388a23d719c8baf4e44db5aed3086e
mim66 <marekmusial2@o2.pl>
2014-11-11 18:49:52 +0100

    MVC_T: 1.15.4 Templates
    Foreach option with Named template

---
+++ _knockoutMVC/ko_knockoutjs_com/Controllers/TemplatesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ko_knockoutjs_com.Controllers
+{
+   public class TemplatesController : Controller
+   {
+      // GET: Templates
+      public ActionResult _01_NamedTemplate()
+      {
+         return View();
+      }
+
+      public ActionResult _02_NamedTempWithForeach()
+      {
+         return View();
+      }
+
+
+      public ActionResult Test_KO()
+      {
+         return View();
+      }
+
+   }
+}
