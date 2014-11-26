2d66393dca119d2641529740cd300beebcc94364
mim66 <marekmusial2@o2.pl>
2014-11-26 16:47:01 +0100

    MVC_T: 1.17.6  02_Filmy
    Filtrowanie wg słów kluczowych i kategorii

--- _MVC5/02_Filmy/Controllers/FilmyController.cs
+++ _MVC5/02_Filmy/Controllers/FilmyController.cs
@@ -10,118 +10,172 @@ using _02_Filmy.Models;
 
 namespace _02_Filmy.Controllers
 {
-    public class FilmyController : Controller
-    {
-        private FilmyDBContext db = new FilmyDBContext();
+   public class FilmyController : Controller
+   {
+      private FilmyDBContext db = new FilmyDBContext();
 
-        // GET: Filmy
-        public ActionResult Index()
-        {
-            return View(db.Filmy.ToList());
-        }
+      // GET: Filmy
+      //// when {controller}/{action}/{id}
+      //public ActionResult Index(string id)
+      //{
+      //   string szukaneSlowo = id; 
+      //   var filmy = from m in db.Filmy
+      //               select m;
+      //   if (!String.IsNullOrEmpty(szukaneSlowo))
+      //   {
+      //      filmy = filmy.Where(f => f.Tytul.Contains(szukaneSlowo));
+      //   }
+      //   return View(filmy);
+      //}
+      //public ActionResult Index(string szukaneSlowo)
+      //{
+      //   var filmy = from m in db.Filmy
+      //               select m;
+      //   if (!String.IsNullOrEmpty(szukaneSlowo))
+      //   {
+      //      filmy = filmy.Where(f => f.Tytul.Contains(szukaneSlowo));
+      //   }
+      //   return View(filmy);
+      //}
+      public ActionResult Index(string kategoriaFilmu, string szukaneSlowo)
+      {
+         var listaKategorii = new List<string>();
 
-        // GET: Filmy/Detale/5
-        public ActionResult Detale(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Film film = db.Filmy.Find(id);
-            if (film == null)
-            {
-                return HttpNotFound();
-            }
-            return View(film);
-        }
+         var query = from d in db.Filmy
+                        orderby d.Kategoria
+                        select d.Kategoria;
 
-        // GET: Filmy/Nowy
-        public ActionResult Nowy()
-        {
-            return View();
-        }
+         listaKategorii.AddRange(query.Distinct());
+         ViewBag.kategoriaFilmu = new SelectList(listaKategorii);
 
-        // POST: Filmy/Nowy
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Nowy([Bind(Include = "ID,Tytul,DataWydania,Kategoria,Cena")] Film film)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Filmy.Add(film);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+         var filmy = from m in db.Filmy
+                      select m;
 
-            return View(film);
-        }
+         if (!String.IsNullOrEmpty(szukaneSlowo))
+         {
+            filmy = filmy.Where(s => s.Tytul.Contains(szukaneSlowo));
+         }
+
+         if (!string.IsNullOrEmpty(kategoriaFilmu))
+         {
+            filmy = filmy.Where(x => x.Kategoria == kategoriaFilmu);
+         }
+
+         return View(filmy);
+      }
+      [HttpPost]
+      public string Index(FormCollection fc, string szukaneSlowo)
+      {
+         return "<h3>Z [HttpPost]Index: " + szukaneSlowo + "</h3>";
+      }
+      //public ActionResult Index()
+      //{
+      //    return View(db.Filmy.ToList());
+      //}
 
 
-        // GET: Filmy/Edycja/5
-        public ActionResult Edycja(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Film film = db.Filmy.Find(id);
-            if (film == null)
-            {
-                return HttpNotFound();
-            }
-            return View(film);
-        }
+      // GET: Filmy/Detale/5
+      public ActionResult Detale(int? id)
+      {
+         if (id == null)
+         {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Film film = db.Filmy.Find(id);
+         if (film == null)
+         {
+            return HttpNotFound();
+         }
+         return View(film);
+      }
 
-        // POST: Filmy/Edycja/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edycja([Bind(Include = "ID,Tytul,DataWydania,Kategoria,Cena")] Film film)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(film).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(film);
-        }
+      // GET: Filmy/Nowy
+      public ActionResult Nowy()
+      {
+         return View();
+      }
 
-        // GET: Filmy/Usun/5
-        public ActionResult Usun(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Film film = db.Filmy.Find(id);
-            if (film == null)
-            {
-                return HttpNotFound();
-            }
-            return View(film);
-        }
-
-        // POST: Filmy/Usun/5
-        [HttpPost, ActionName("Usun")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Film film = db.Filmy.Find(id);
-            db.Filmy.Remove(film);
+      // POST: Filmy/Nowy
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+      // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Nowy([Bind(Include = "ID,Tytul,DataWydania,Kategoria,Cena")] Film film)
+      {
+         if (ModelState.IsValid)
+         {
+            db.Filmy.Add(film);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+         return View(film);
+      }
+
+
+      // GET: Filmy/Edycja/5
+      public ActionResult Edycja(int? id)
+      {
+         if (id == null)
+         {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Film film = db.Filmy.Find(id);
+         if (film == null)
+         {
+            return HttpNotFound();
+         }
+         return View(film);
+      }
+
+      // POST: Filmy/Edycja/5
+      // To protect from overposting attacks, please enable the specific properties you want to bind to, for         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult Edycja([Bind(Include = "ID,Tytul,DataWydania,Kategoria,Cena")] Film film)
+      {
+         if (ModelState.IsValid)
+         {
+            db.Entry(film).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(film);
+      }
+
+      // GET: Filmy/Usun/5
+      public ActionResult Usun(int? id)
+      {
+         if (id == null)
+         {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+         }
+         Film film = db.Filmy.Find(id);
+         if (film == null)
+         {
+            return HttpNotFound();
+         }
+         return View(film);
+      }
+
+      // POST: Filmy/Usun/5
+      [HttpPost, ActionName("Usun")]
+      [ValidateAntiForgeryToken]
+      public ActionResult DeleteConfirmed(int id)
+      {
+         Film film = db.Filmy.Find(id);
+         db.Filmy.Remove(film);
+         db.SaveChanges();
+         return RedirectToAction("Index");
+      }
+
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing)
+         {
+            db.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+   }
 }
