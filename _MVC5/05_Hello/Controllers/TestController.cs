0d26bfbac9dc5a66d226e6a9daed93781c08bd15
mim66 <marekmusial2@o2.pl>
2015-09-27 05:52:49 +0200

    MVC5_T: 1.22.4 MVC5: Hello_2day_3ViewModel

--- _MVC5/05_Hello/Controllers/TestController.cs
+++ _MVC5/05_Hello/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using _05_Hello.Models;
+using _05_Hello.ViewModel;
 
 namespace _05_Hello.Controllers
 {
@@ -12,20 +13,44 @@ namespace _05_Hello.Controllers
    {
 
       public ActionResult GetEmployeeViewModel() {
-         Employee emp = NewEmployee();
-         
-         ViewModel.EmployeeViewModel vmEmp = new ViewModel.EmployeeViewModel();
-         vmEmp.EmployeeName = emp.FirstName +" "+ emp.FirstName;
-         vmEmp.Salary = emp.Salary.ToString("C");
-         if (emp.Salary > 15000) { 
-            vmEmp.SalaryColor = "yellow";
+         EmployeeListViewModel empListViewModel = new EmployeeListViewModel();
+         EmployeeBusinessLayer empBal           = new EmployeeBusinessLayer();
+         List<Employee> employees               = empBal.GetEmployees();
+         List<EmployeeViewModel> empViewModels  = new List<EmployeeViewModel>();
+
+         foreach (Employee emp in employees) {
+            EmployeeViewModel empViewModel = new EmployeeViewModel();
+            empViewModel.EmployeeName = emp.FirstName +" "+emp.LastName;
+            empViewModel.Salary = emp.Salary.ToString("C");
+            if (emp.Salary > 15000) { 
+               empViewModel.SalaryColor = "yellow";
+            }
+            else {
+               empViewModel.SalaryColor = "green";
+            }
+            empViewModels.Add(empViewModel);
          }
-         else { 
-            vmEmp.SalaryColor = "green";
-         }
-         vmEmp.UserName = "Admin";
-         return View(vmEmp);
+         empListViewModel.Employees = empViewModels;
+         empListViewModel.UserName  = "Admin";
+
+         return View(empListViewModel);
       }
+
+      //public ActionResult GetEmployeeViewModel() {
+      //   Employee emp = NewEmployee();
+         
+      //   ViewModel.EmployeeViewModel vmEmp = new ViewModel.EmployeeViewModel();
+      //   vmEmp.EmployeeName = emp.FirstName +" "+ emp.FirstName;
+      //   vmEmp.Salary = emp.Salary.ToString("C");
+      //   if (emp.Salary > 15000) { 
+      //      vmEmp.SalaryColor = "yellow";
+      //   }
+      //   else { 
+      //      vmEmp.SalaryColor = "green";
+      //   }
+      //   //vmEmp.UserName = "Admin";
+      //   return View(vmEmp);
+      //}
 
 
       public ActionResult GetStronglyTypedView() {
--- _MVC5/05_Hello/ViewModel/EmployeeViewModel.cs
+++ _MVC5/05_Hello/ViewModel/EmployeeViewModel.cs
@@ -10,6 +10,6 @@ namespace _05_Hello.ViewModel
       public string EmployeeName { get; set; }
       public string Salary { get; set; }
       public string SalaryColor { get; set; }
-      public string UserName { get; set; }
+      //public string UserName { get; set; }
    }
 }
