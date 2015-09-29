3a8691fe6b056a1b75d7be1b0eeb77bf1620bee9
mim66 <marekmusial2@o2.pl>
2015-09-29 14:26:40 +0200

    MVC5_T: 1.22.4 MVC5: Hello_3day: Enable-Migration z zasileniem tabeli

---
+++ _MVC5/05_Hello/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+namespace _05_Hello.Migrations
+{
+   using _05_Hello.Models;
+   using System.Data.Entity.Migrations;
+
+   internal sealed class Configuration : DbMigrationsConfiguration<_05_Hello.DAL.MvcHelloDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(_05_Hello.DAL.MvcHelloDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+           context.Pracownicy.AddOrUpdate(
+             p => p.PracId,
+             new Pracownik { Imie = "jonson", Nazwisko = "fernandes", Pensja = 14000 },
+             new Pracownik { Imie = "michael", Nazwisko = "jackson", Pensja = 16000 },
+             new Pracownik { Imie = "robert", Nazwisko = "pattinson", Pensja = 20000 }
+            );
+
+        }
+    }
+}
