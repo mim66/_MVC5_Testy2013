5d7297390cf9e06090a3420de638db7804917ef1
mim66 <marekmusial2@o2.pl>
2015-10-11 17:39:15 +0200

    MVC5_T: 1.22.7 mvc5_Hello_14_ Walidacja niestandardowa atrubutu

--- _MVC5/05_Hello/Models/Pracownik.cs
+++ _MVC5/05_Hello/Models/Pracownik.cs
@@ -11,12 +11,28 @@ namespace _05_Hello.Models
       [Key]
       public int PracId { get; set; }
       
-      [Required(ErrorMessage="Proszę podać imię:")]
+      // walidacja niestandardowa
+      [Imie_Val]
       public string Imie         { get; set; }
 
       [Required(ErrorMessage = "Proszę podać nazwisko:")]
       [StringLength(30, ErrorMessage = "Długość nazwiska nie może być większa niż 30 znaków.")]
       public string Nazwisko { get; set; }
       public int Pensja   { get; set; }
+   }
+   
+   public class Imie_Val: ValidationAttribute
+   {
+      protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+         if (value == null) {
+            return new ValidationResult("Prosze podać imię:");
+         }
+         else {
+            if (value.ToString().Contains("@")) {
+               return new ValidationResult("Imię nie może zawierać znaku '@'...");
+            }
+         }
+         return ValidationResult.Success;
+      }
    }
 }
