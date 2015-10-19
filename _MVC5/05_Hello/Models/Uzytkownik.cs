4dde37c56079232a193085f4a5fc8cf340899d64
mim66 <marekmusial2@o2.pl>
2015-10-19 16:24:16 +0200

    MVC5_T: 1.22.21 mvc5_Hello Client side validation by Microsoft.jQuery.Unobtrusive.Validation

---
+++ _MVC5/05_Hello/Models/Uzytkownik.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _05_Hello.Models
+{
+   public class Uzytkownik
+   {
+      [StringLength(7, MinimumLength=2, ErrorMessage="Długośc nazwy użytkownika powinna być w przedziale od 2 do 7 znaków...")]
+      public string NazwaUsera   { get; set; }
+      public string Haslo        { get; set; }
+   }
+}
